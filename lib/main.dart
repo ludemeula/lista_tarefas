7e8d0a1c83bb062345995f9e5caa6782784678ad
ludemeula <ludemeula@gmail.com>
2019-06-28 16:34:15 -0300

    Adding inputs to the layout

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,40 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Lista de Tarefas',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xff4fc3f7),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(
+                          labelText: 'Nova Tarefa',
+                          helperText: 'Insira a Nova Tarefa'),
+                      style: TextStyle(fontSize: 20)),
+                ),
+                RaisedButton(
+                  child: Text('ADD'),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Future<File> _getFile() async {
